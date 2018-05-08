183812513dc3b369bee133e90abb316b4e257de9
Catherine <cathey191@hotmail.com>
2018-05-08 16:15:51 +1200

    One question - not working

--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@ $(document).ready(function(){
 	var passArray = [];
 	var failArray = [];
 	var question = document.getElementById('question');
-	var answer = document.getElementById('input').value;
+	// var answer = document.getElementById('input').value;
 	// var button = document.getElementById('button');
 
-	function counter(){
+	$('.button').click(function(){
 		console.log('test');
-		// var answer = document.getElementById('input').value;
+		var answer = document.getElementById('input').value;
+
+		$('.correct').text(passNumber);
+		$('.incorrect').text(failNumber);
+		$('.total').text(totalNumber);
+
+		if (answer == 'Wellington') {
+			passArray.push(answer);
+			console.dir(passArray);
+			console.log('pass')
+		} else {
+			failArray.push(answer);
+			// console.log('fail')
+		}
+
 		//number of answers
 		var passWords = passArray.length;
 		var failWords = failArray.length;
@@ -18,57 +32,5 @@ $(document).ready(function(){
 		var totalWords = passWords + failWords
 		var totalNumber = totalWords.toString();
 
-		$('.correct').text(passNumber);
-		$('.incorrect').text(failNumber);
-		$('.total').text(totalNumber);
-	};
-
-	$('.firButton').click(function(){
-		// var answer = document.getElementById('input').value;
-		counter();
-
-		if (answer == 'Wellington') {
-			passArray.push(answer);
-			console.log('pass')
-		} else {
-			failArray.push(answer);
-			// console.log('fail')
-		}
-		$('#question').text('Which country has the largest surface area in the world?');
-		$('#button').removeClass('firButton');
-		$('#button').addClass('secButton');
 	});
-
-	$('.secButton').click(function(){
-		// var answer = document.getElementById('input').value;
-		counter();
-
-		if (answer == 'Russia') {
-			passArray.push(answer);
-			// console.log('pass')
-		} else {
-			failArray.push(answer);
-			// console.log('fail')
-		}
-		$('#question').text('one plus one?');
-		$('#button').removeClass('secButton');
-		$('#button').addClass('thrButton');
-	});
-
-	$('.thrButton').click(function(){
-		// var answer = document.getElementById('input').value;
-		counter();
-
-		if (answer == 'Two') {
-			passArray.push(answer);
-			// console.log('pass')
-		} else {
-			failArray.push(answer);
-			// console.log('fail')
-		}
-		$('#question').text('test');
-		$('#button').removeClass('thrButton');
-		$('#button').addClass('fouButton');
-	});
-
 });
